6f7738618f49b372b4a693ba11cc92201eee63fe
Vlad2226 <59125024+vlad2226@users.noreply.github.com>
2021-10-14 19:22:08 +0300

    new deploy

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 // const firebaseApp = initializeApp({});
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAoqGyxoVfAq047AxoY-sIe6-xd250B9RQ",
-  authDomain: "asigurare-e01af.firebaseapp.com",
-  projectId: "asigurare-e01af",
-  storageBucket: "asigurare-e01af.appspot.com",
-  messagingSenderId: "666804645243",
-  appId: "1:666804645243:web:8bd391ae718f4d0a6b21de",
-  measurementId: "G-5Q6334RNMR",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAoqGyxoVfAq047AxoY-sIe6-xd250B9RQ",
+//   authDomain: "asigurare-e01af.firebaseapp.com",
+//   projectId: "asigurare-e01af",
+//   storageBucket: "asigurare-e01af.appspot.com",
+//   messagingSenderId: "666804645243",
+//   appId: "1:666804645243:web:8bd391ae718f4d0a6b21de",
+//   measurementId: "G-5Q6334RNMR",
+// };
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,6 +21,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
